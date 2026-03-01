58d7213635c8074b87e427869835198a234db199
1226lea <wangyaqi20241@163.com>
2026-03-01 23:25:00 +0000

    Testing framework implemented and media URL patterns fixed

--- core/tests.py
+++ core/tests.py
@@ -1,3 +1,9 @@
 from django.test import TestCase
+from .models import Category
 
-# Create your tests here.
+
+class CategoryModelTest(TestCase):
+    def test_category_creation(self):
+
+        cat = Category.objects.create(name="Lecture Notes")
+        self.assertEqual(str(cat), "Lecture Notes")
