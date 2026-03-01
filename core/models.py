eaef1469318e624fd65e9ba7b3de1aa26aa2185b
1226lea <wangyaqi20241@163.com>
2026-03-01 19:32:20 +0000

    Finalize database migrations and core model architecture

--- core/models.py
+++ core/models.py
@@ -1,3 +1,25 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name
+
+
+class LearningResource(models.Model):
+    title = models.CharField(max_length=50)
+    description = models.TextField()
+
+    url = models.URLField(max_length=500, blank=True, null=True)
+    file = models.FileField(upload_to='resources/', blank=True, null=True)
+    upload_time = models.DateTimeField(auto_now_add=True)
+
+    uploader = models.ForeignKey(User, on_delete=models.CASCADE)
+    category = models.ForeignKey(
+        Category, on_delete=models.SET_NULL, null=True)
+
+    def __str__(self):
+        return self.title
--- core/views.py
+++ core/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import LearningResource
 
-# Create your views here.
+
+def resource_list(request):
+
+    query = request.GET.get('search')
+    if query:
+        resources = LearningResource.objects.filter(title__icontains=query)
+    else:
+        resources = LearningResource.objects.all().order_by('-upload_time')
+
+    return render(request, 'core/resource_list.html', {'resources': resources})
