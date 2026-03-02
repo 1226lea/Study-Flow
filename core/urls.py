6d3bd89d1d1f32c794f06d12632756c8cda6311c
1226lea <wangyaqi20241@163.com>
2026-03-02 21:05:16 +0000

    Implement core business logic: Resource Upload View

--- core/urls.py
+++ core/urls.py
@@ -7,6 +7,7 @@ urlpatterns = [
 
     path('login/', auth_views.LoginView.as_view(template_name='core/login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
-    
+
     path('register/', views.register_view, name='register'),
+    path('upload/', views.upload_resource, name='upload_resource'),
 ]
--- core/views.py
+++ core/views.py
@@ -1,3 +1,5 @@
+from django.contrib.auth.decorators import login_required
+from .models import Category, LearningResource
 from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth import login
@@ -16,21 +18,49 @@ def resource_list(request):
     return render(request, 'core/resource_list.html', {'resources': resources})
 
 
-
 def register_view(request):
-    
+
     if request.user.is_authenticated:
-        return redirect('resource_list') 
-        
+        return redirect('resource_list')
+
     if request.method == 'POST':
         form = UserCreationForm(request.POST)
         if form.is_valid():
             user = form.save()
-            login(request, user) 
-            messages.success(request, f"Welcome {user.username}! Account created.")
-            
+            login(request, user)
+            messages.success(
+                request, f"Welcome {user.username}! Account created.")
+
             return redirect('resource_list')
     else:
         form = UserCreationForm()
-        
+
     return render(request, 'core/register.html', {'form': form})
+
+
+@login_required
+def upload_resource(request):
+    if request.method == 'POST':
+
+        title = request.POST.get('title')
+        description = request.POST.get('description')
+        category_id = request.POST.get('category')
+
+        resource_file = request.FILES.get('resource_file')
+
+        if title and resource_file:
+
+            LearningResource.objects.create(
+                title=title,
+                description=description,
+                category_id=category_id,
+                file=resource_file,
+                uploader=request.user
+            )
+            messages.success(request, "Resource uploaded successfully!")
+            return redirect('resource_list')
+        else:
+            messages.error(request, "Title and File are required.")
+
+    categories = Category.objects.all()
+    return render(request, 'core/upload.html', {'categories': categories})
