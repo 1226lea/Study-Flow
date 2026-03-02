d51e5b2e100b03c961130020452fad2149db34cd
1226lea <wangyaqi20241@163.com>
2026-03-02 21:27:13 +0000

    Final project core logic: Upload and List functional and verified

--- core/tests.py
+++ core/tests.py
@@ -1,9 +1,25 @@
-from django.test import TestCase
-from .models import Category
+from django.test import TestCase, Client
+from django.urls import reverse
+from .models import Category, LearningResource
 
 
-class CategoryModelTest(TestCase):
+class StudyFlowTest(TestCase):
+    def setUp(self):
+
+        self.category = Category.objects.create(name="Lecture Notes")
+        self.client = Client()
+
     def test_category_creation(self):
 
-        cat = Category.objects.create(name="Lecture Notes")
-        self.assertEqual(str(cat), "Lecture Notes")
+        self.assertEqual(str(self.category), "Lecture Notes")
+
+    def test_resource_list_view(self):
+
+        response = self.client.get(reverse('resource_list'))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'core/resource_list.html')
+
+    def test_upload_redirect_if_not_logged_in(self):
+
+        response = self.client.get(reverse('upload_resource'))
+        self.assertEqual(response.status_code, 302)
